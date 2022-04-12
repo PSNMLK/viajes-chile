00c0c56d5bb60f22af0d765fbae45f680f8fca12
PAI <olbapemp@hotmail.es>
2022-04-12 18:24:50 -0400

    scripts and finals issues

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,52 @@
+
+$(document).ready(function(){
+
+
+  $("a").click(function(){
+    var info = this.hash
+    
+    $("html, body").animate(
+      {
+        scrollTop: $(info).offset().top -70
+      },
+      2000
+      
+      )
+
+})
+
+})
+
+
+$(function(){
+  $(window).scroll(function(){
+    $('#Navegacion').offset().top
+    console.log()
+    if(window.innerWidth<453){
+      if($('#Navegacion').offset().top >271){
+        $('#Navegacion').addClass('bg-info')
+  
+  
+      }
+      else{
+        $('#Navegacion').removeClass('bg-info')
+      }
+
+    } else{
+    console.log($('#Navegacion').offset().top)
+    if($('#Navegacion').offset().top >558){
+      $('#Navegacion').addClass('bg-info')
+
+
+    }
+    else{
+      $('#Navegacion').removeClass('bg-info')
+    }
+  }
+  })
+
+})
+
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
